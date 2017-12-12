2066105e350e289829f6781a2815cb6a7c18413d
peter9959 <peterekner@gmail.com>
2017-12-12 11:37:20 +0100

    day5-more

--- AdventConsole/Day1/Day5Solution.cs
+++ AdventConsole/Day1/Day5Solution.cs
@@ -11,7 +11,23 @@ namespace AdventConsole.Day1
     {
         private string _input = File.ReadAllText(@"../../Input/day5_input.txt");
         private string _input2 = File.ReadAllText(@"../../Input/day5_test_input.txt");
+        //        For example, consider the following list of jump offsets:
 
+        //0
+        //3
+        //0
+        //1
+        //-3
+        //Positive jumps("forward") move downward; negative jumps move upward.For legibility in this example, these offset values will be written all on one line, with the current instruction marked in parentheses.The following steps would be taken before an exit is found:
+
+
+        //(0) 3  0  1  -3  - before we have taken any steps.
+        //(1) 3  0  1  -3  - jump with offset 0 (that is, don't jump at all). Fortunately, the instruction is then incremented to 1.
+        // 2 (3) 0  1  -3  - step forward because of the instruction we just modified.The first instruction is incremented again, now to 2.
+        // 2  4  0  1 (-3) - jump all the way to the end; leave a 4 behind.
+        // 2 (4) 0  1  -2  - go back to where we just were; increment -3 to -2.
+        // 2  5  0  1  -2  - jump 4 steps forward, escaping the maze.
+        //In this example, the exit is reached in 5 steps.
         public int CalculateFirstAnswer()
         {
             return CalculateFirstAnswer(_input);
@@ -27,45 +43,31 @@ namespace AdventConsole.Day1
 
             int sum = 0;
 
-            int offset = 0;
-            //int stepsToJump = 0;
-
-            DoStuff(0, 0);
-
+            var sum2 = DoStuff(0);
+            
             //do stuff
-            void DoStuff(int inIndex, int stepsToJump)
+            int DoStuff(int inIndex)
             {
-                var jump = 0;
-                if (inIndex < 0)
+                if (inIndex <= intValues.Length - 1)
                 {
-                    jump = intValues[stepsToJump  + inIndex];
-                    intValues[stepsToJump] += 1;
-
-                    sum += 1;
-                    DoStuff(stepsToJump + inIndex, jump);
-                }
-                else
-                {
-                    jump = intValues[inIndex];
+                    var val = intValues[inIndex];
+                    //jump
                     intValues[inIndex] += 1;
+
+                    var localNextIndex = inIndex + val;
+                    sum += 1;
+
+                    if(sum < 63000)
+                        DoStuff(localNextIndex);
+
                 }
 
-
-                //-3
-                sum += 1;
-                try
-                {
-                    DoStuff(intValues[inIndex + jump], inIndex + jump);
-                }
-                catch (Exception e)
-                {
-                    //done        
-                }
+                return sum;
             }
 
 
 
-            
+
 
 
             //for (int i = 0; i < rows.Length; i++)
